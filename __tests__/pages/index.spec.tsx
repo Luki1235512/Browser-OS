3c3315d8ff18fd0a0deb4a7cfd27f91c6ec628e0
Luki1235512 <Luki1235512@gmail.com>
2023-03-03 15:31:51 +0100

    prettier

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -2,8 +2,8 @@ import { render } from '@testing-library/react';
 import Index from 'pages/index';
 
 test('renders index page', () => {
-    const { getByText } = render(<Index />);
-    const helloWorldElement = getByText('Hello, World!');
+  const { getByText } = render(<Index />);
+  const helloWorldElement = getByText('Hello, World!');
 
-    expect(helloWorldElement).toBeInTheDocument();
+  expect(helloWorldElement).toBeInTheDocument();
 });
