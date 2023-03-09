2c0fdffc6e8acb605b307b15fa6970a0f9fb0c8d
Luki1235512 <Luki1235512@gmail.com>
2023-03-09 13:52:15 +0100

    Animated Desktop Wallpaper

--- styles/themes.ts
+++ styles/themes.ts
@@ -1,11 +1,19 @@
 import type { Themes } from 'types/styles/themes';
+import vantaWaves from 'utils/vantaWaves';
 
 const defaultTheme = {
   colors: {
     backgroundcolor: '#ffc0cb',
     primary: '#ffc0cb',
     window: '#fdeef4'
-  }
+  },
+  wallpaper: vantaWaves({
+    color: 0xbe4da7,
+    shininess: 30,
+    waveHeight: 15,
+    waveSpeed: 0.7,
+    zoom: 1
+  })
 };
 
 const themes: Themes = { defaultTheme };
