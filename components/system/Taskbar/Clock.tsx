f3705ee6a3a748018ef2399ecdf2acd111b05ef7
Luki1235512 <Luki1235512@gmail.com>
2023-03-10 12:05:54 +0100

    Reorganized files and folders

--- components/system/Taskbar/Clock.tsx
+++ components/system/Taskbar/Clock.tsx
@@ -1,4 +1,4 @@
-import StyledClock from 'styles/components/system/StyledClock';
+import StyledClock from 'styles/components/system/Taskbar/StyledClock';
 
 const Clock: React.FC = () => <StyledClock>21:37</StyledClock>;
 
