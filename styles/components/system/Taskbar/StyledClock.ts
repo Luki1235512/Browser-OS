3782bc48601ae2bd9502bfc5f764e0bc95d43604
Luki1235512 <Luki1235512@gmail.com>
2023-03-12 22:37:43 +0100

    Adjusted clock font size

--- styles/components/system/Taskbar/StyledClock.ts
+++ styles/components/system/Taskbar/StyledClock.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const StyledClock = styled.time`
   height: 100%;
   width: ${({ theme }) => theme.sizes.clock.width};
+  font-size: ${({ theme }) => theme.fonts.clock.size};
   background-color: red;
   position: absolute;
   display: flex;
