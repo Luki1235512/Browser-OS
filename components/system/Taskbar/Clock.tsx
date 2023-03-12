f8f7dd25f29e237741d40f85fe20c423cc8af1fb
Luki1235512 <Luki1235512@gmail.com>
2023-03-12 22:21:26 +0100

    Only using React.FC when sensible

--- components/system/Taskbar/Clock.tsx
+++ components/system/Taskbar/Clock.tsx
@@ -1,5 +1,7 @@
 import StyledClock from 'styles/components/system/Taskbar/StyledClock';
 
-const Clock: React.FC = () => <StyledClock dateTime="21:37">21:37</StyledClock>;
+const Clock = (): JSX.Element => (
+  <StyledClock dateTime="21:37">21:37</StyledClock>
+);
 
 export default Clock;
