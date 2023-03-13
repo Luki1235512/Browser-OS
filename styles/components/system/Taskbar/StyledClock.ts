d68f8a50d00dedbe9ccaf9ad3d1809c14c783751
Luki1235512 <Luki1235512@gmail.com>
2023-03-13 11:42:46 +0100

    Changed clock text color

--- styles/components/system/Taskbar/StyledClock.ts
+++ styles/components/system/Taskbar/StyledClock.ts
@@ -4,6 +4,7 @@ const StyledClock = styled.time`
   height: 100%;
   width: ${({ theme }) => theme.sizes.clock.width};
   font-size: ${({ theme }) => theme.sizes.clock.fontSize};
+  color: ${({ theme }) => theme.colors.clockText};
   position: absolute;
   display: flex;
   place-content: center;
