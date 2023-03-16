73a222cf305d15fe40483a61cd914165ba53c594
Luki1235512 <Luki1235512@gmail.com>
2023-03-16 10:29:31 +0100

    Extracted out Windows icon and removed font awesome

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -1,10 +1,9 @@
-import { faPaw } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { WindowsIcon } from 'components/system/Icons';
 import StyledStartButton from 'styles/components/system/Taskbar/StyledStartButton';
 
 const StartButton = (): JSX.Element => (
   <StyledStartButton title="Start">
-    <FontAwesomeIcon icon={faPaw} />
+    <WindowsIcon />
   </StyledStartButton>
 );
 
--- styles/components/system/Taskbar/StyledStartButton.ts
+++ styles/components/system/Taskbar/StyledStartButton.ts
@@ -4,13 +4,16 @@ import Button from 'styles/common/Button';
 const StyledStartButton = styled(Button)`
   height: 100%;
   width: ${({ theme }) => theme.sizes.startButton.width};
-  font-size: ${({ theme }) => theme.sizes.startButton.iconSize};
-  color: ${({ theme }) => theme.colors.startButton};
+  fill: ${({ theme }) => theme.colors.startButton};
   position: absolute;
   display: flex;
   place-content: center;
   place-items: center;
   left: 0;
+
+  svg {
+    height: 19px;
+  }
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.taskbarHover};
