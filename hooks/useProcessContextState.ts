c5874f746a06d0aeb8e33b6a6da93f2db4425c45
Luki1235512 <Luki1235512@gmail.com>
2023-03-22 10:31:31 +0100

    Allowed running urls in apps and multiple instances of app

--- hooks/useProcessContextState.ts
+++ hooks/useProcessContextState.ts
@@ -13,7 +13,7 @@ type ProcessesMap = (
 
 export type ProcessContextState = {
   close: (id: string) => void;
-  open: (id: string) => void;
+  open: (id: string, url: string) => void;
   mapProcesses: ProcessesMap;
   maximize: (id: string) => void;
   minimize: (id: string) => void;
@@ -28,7 +28,10 @@ const useProcessContextState = (): ProcessContextState => {
   );
 
   const close = useCallback((id: string) => setProcesses(closeProcess(id)), []);
-  const open = useCallback((id: string) => setProcesses(openProcess(id)), []);
+  const open = useCallback(
+    (id: string, url: string) => setProcesses(openProcess(id, url)),
+    []
+  );
 
   const maximize = useCallback(
     (id: string) => setProcesses(maximizeProcess(id)),
