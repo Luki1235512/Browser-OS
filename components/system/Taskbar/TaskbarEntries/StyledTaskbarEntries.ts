61e33a577b74aaaefe09e11f373edecd5392dc7f
Luki1235512 <Luki1235512@gmail.com>
2023-03-23 14:49:30 +0100

    Tweaks to taskbar entries

--- components/system/Taskbar/TaskbarEntries/StyledTaskbarEntries.ts
+++ components/system/Taskbar/TaskbarEntries/StyledTaskbarEntries.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 const StyledTaskbarEntries = styled.ol`
+  column-gap: 1px;
   display: flex;
   height: 100%;
-  position: absolute;
   left: ${({ theme }) => theme.sizes.startButton.width};
+  margin: 0 3px;
+  position: absolute;
   right: ${({ theme }) => theme.sizes.clock.width};
 `;
 
