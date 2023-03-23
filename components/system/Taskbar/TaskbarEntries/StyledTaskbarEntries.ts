948421eb48cb1630912c819d8e628e61f97cd212
Luki1235512 <Luki1235512@gmail.com>
2023-03-23 14:39:37 +0100

    Show taskbar entries left to right

--- components/system/Taskbar/TaskbarEntries/StyledTaskbarEntries.ts
+++ components/system/Taskbar/TaskbarEntries/StyledTaskbarEntries.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const StyledTaskbarEntries = styled.ol`
+  display: flex;
   height: 100%;
   position: absolute;
   left: ${({ theme }) => theme.sizes.startButton.width};
