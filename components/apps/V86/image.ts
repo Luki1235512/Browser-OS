571dcf0ea810f968510824d2924ee2552134a7e3
Luki1235512 <Luki1235512@gmail.com>
2023-03-30 11:52:23 +0200

    v86 floppy detection

---
+++ components/apps/V86/image.ts
@@ -0,0 +1,20 @@
+type V86Image = {
+  async?: boolean;
+  size?: number;
+  url: string;
+  use_parts?: boolean;
+};
+
+type V86ImageType = 'cdrom' | 'fda' | 'hda';
+
+export type V86ImageConfig = Partial<Record<V86ImageType, V86Image>>;
+
+const SUPPORTED_FLOPPY_TYPES = [
+  160, 180, 200, 320, 360, 400, 720, 1200, 1440, 1722, 2880
+];
+
+const isFloppyImage = (size: number): boolean =>
+  SUPPORTED_FLOPPY_TYPES.includes(size >> 10) && (size & 0x3ff) === 0;
+
+export const getImageType = (size: number): string =>
+  isFloppyImage(size) ? 'fda' : 'hda';
