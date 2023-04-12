fa3cf3dfacd74f670f649264591d032f46903c6b
Luki1235512 <Luki1235512@gmail.com>
2023-04-12 11:32:52 +0200

    Added the Sidebar

---
+++ components/system/StartMenu/Sidebar/StyledSidebarButton.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+type StyledSidebarButtonProps = {
+  active?: boolean;
+};
+
+const StyledSidebarButton = styled.li<StyledSidebarButtonProps>`
+  border: 1px solid transparent;
+  display: flex;
+  height: 48px;
+  place-content: center;
+  place-items: center;
+  width: 48px;
+
+  &::before {
+    border-left: ${({ active, theme }) =>
+      `4px solid ${active ? theme.colors.highlight : 'transparent'}`};
+    content: '';
+    height: 48px;
+    position: absolute;
+    width: 48px;
+  }
+
+  figure {
+    color: ${({ active, theme }) =>
+      active ? theme.colors.highlight : theme.colors.text};
+
+    svg {
+      fill: ${({ active, theme }) =>
+        active ? theme.colors.highlight : theme.colors.text};
+      height: 16px;
+      margin-bottom: -1px;
+      margin-left: 1px;
+      width: 16px;
+    }
+
+    figcaption {
+      display: none;
+    }
+  }
+
+  &:hover {
+    background-color: hsla(300, 100%, 40%, 40%);
+    border: 1px solid hsla(300, 100%, 40%, 70%);
+  }
+`;
+
+export default StyledSidebarButton;
--- components/system/StartMenu/StyledStartMenu.ts
+++ components/system/StartMenu/StyledStartMenu.ts
@@ -4,6 +4,7 @@ const StyledStartMenu = styled.nav`
   backdrop-filter: blur(12px);
   background-color: hsla(300, 100%, 60%, 40%);
   bottom: ${({ theme }) => theme.sizes.taskbar.height};
+  display: flex;
   height: 350px;
   left: 0;
   position: absolute;
