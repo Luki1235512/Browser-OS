2df79b0aaf69383e0add08f97c21be5a3541b4d1
Luki1235512 <Luki1235512@gmail.com>
2023-04-18 18:23:14 +0200

    Added Window Peak

---
+++ components/system/Taskbar/TaskbarEntry/Peak/StyledPeakWindow.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const StyledPeakWindow = styled.div`
+  background-color: ${({ theme }) => theme.colors.taskbar.hover};
+  border: ${({ theme }) => `1px solid ${theme.colors.taskbar.background}`};
+  border-bottom: 0;
+  bottom: ${({ theme }) => theme.sizes.taskbar.height};
+  display: flex;
+  place-content: center;
+  place-items: flex-start;
+  position: absolute;
+  width: 150px;
+
+  img {
+    padding: 8px;
+    width: 100%;
+  }
+
+  button {
+    background-color: ${({ theme }) => theme.colors.titleBar.background};
+    height: 32px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 32px;
+
+    svg {
+      fill: ${({ theme }) => theme.colors.titleBar.text};
+      width: 12px;
+    }
+
+    &:active {
+      background-color: rgb(160, 30, 160);
+    }
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.titleBar.backgroundHover};
+    }
+  }
+`;
+
+export default StyledPeakWindow;
