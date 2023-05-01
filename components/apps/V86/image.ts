d175b6bc82c568fdb43999f04683d13fd9437dd2
Luki1235512 <Luki1235512@gmail.com>
2023-05-01 15:45:54 +0200

    Use `has` instead of `includes` with Sets

--- components/apps/V86/image.ts
+++ components/apps/V86/image.ts
@@ -9,12 +9,12 @@ type V86ImageType = "cdrom" | "fda" | "hda";
 
 export type V86ImageConfig = Partial<Record<V86ImageType, V86Image>>;
 
-const SUPPORTED_FLOPPY_TYPES = [
+const SUPPORTED_FLOPPY_TYPES = new Set([
   160, 180, 200, 320, 360, 400, 720, 1200, 1440, 1722, 2880,
-];
+]);
 
 const isFloppyImage = (size: number): boolean =>
-  SUPPORTED_FLOPPY_TYPES.includes(size >> 10) && (size & 0x3ff) === 0;
+  SUPPORTED_FLOPPY_TYPES.has(size >> 10) && (size & 0x3ff) === 0;
 
 export const getImageType = (size: number): string =>
   isFloppyImage(size) ? "fda" : "hda";
