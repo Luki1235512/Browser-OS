91a2cc1afc1eefa339cd84b71ddfca28a53cc38a
Luki1235512 <Luki1235512@gmail.com>
2023-05-03 13:56:33 +0200

    Refactored useRnd sub hooks

--- components/system/Window/RndWindow/useDraggable.ts
+++ components/system/Window/RndWindow/useDraggable.ts
@@ -1,12 +1,25 @@
+import { centerPosition } from "components/system/Window/functions";
+import { useSession } from "contexts/session";
 import { useState } from "react";
 import type { Position, Props } from "react-rnd";
-import { DEFAULT_WINDOW_POSITION } from "utils/constants";
+import { useTheme } from "styled-components";
 
 export type Size = NonNullable<Props["size"]>;
 
 type Draggable = [Position, React.Dispatch<React.SetStateAction<Position>>];
 
-const useDraggable = (position = DEFAULT_WINDOW_POSITION): Draggable => {
+const useDraggable = (id: string, size: Size): Draggable => {
+  const {
+    sizes: {
+      taskbar: { height: taskbarHeight },
+    },
+  } = useTheme();
+  const {
+    windowStates: {
+      [id]: { position = centerPosition(size, taskbarHeight) } = {},
+    },
+  } = useSession();
+
   const [{ x, y }, setPosition] = useState<Position>(position);
 
   return [{ x, y }, setPosition];
