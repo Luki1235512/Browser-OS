485c044d4d96792f31e6b6be1b0659d5c8c1fc48
Luki1235512 <Luki1235512@gmail.com>
2023-05-12 14:55:10 +0200

    Use inset when possible

--- components/system/Desktop/StyledDesktop.ts
+++ components/system/Desktop/StyledDesktop.ts
@@ -1,14 +1,11 @@
 import styled from "styled-components";
 
 const StyledDesktop = styled.main`
-  height: 100vh;
-  width: 100vw;
   background-color: ${({ theme }) => theme.colors.background};
+  height: 100vh;
+  inset: 0;
   position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  width: 100vw;
 `;
 
 export default StyledDesktop;
