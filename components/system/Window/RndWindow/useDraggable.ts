d912ea4c47ce4ca68c9a8348f9f57ac5a2d6230e
Luki1235512 <Luki1235512@gmail.com>
2023-05-16 11:43:17 +0200

    Keep window in desktop bounds

--- components/system/Window/RndWindow/useDraggable.ts
+++ components/system/Window/RndWindow/useDraggable.ts
@@ -1,13 +1,14 @@
 import {
   cascadePosition,
   centerPosition,
+  isWindowOutsideBounds,
 } from "components/system/Window/functions";
 import { useProcesses } from "contexts/process";
 import { useSession } from "contexts/session";
 import { useState } from "react";
 import type { Position, Props } from "react-rnd";
 import { useTheme } from "styled-components";
-import { isRectOutsideWindow, pxToNum } from "utils/functions";
+import { pxToNum } from "utils/functions";
 
 export type Size = NonNullable<Props["size"]>;
 
@@ -22,13 +23,11 @@ const useDraggable = (id: string, size: Size): Draggable => {
   } = useTheme();
   const { processes } = useProcesses();
   const { stackOrder, windowStates: { [id]: windowState } = {} } = useSession();
-  const { position, size: windowSize } = windowState || {};
-  const isOffscreen = isRectOutsideWindow(
-    position?.x,
-    position?.y,
-    pxToNum(windowSize?.height),
-    pxToNum(windowSize?.width)
-  );
+  const { position } = windowState || {};
+  const isOffscreen = isWindowOutsideBounds(windowState, {
+    x: window?.innerWidth || 0,
+    y: (window?.innerHeight || 0) - pxToNum(taskbarHeight),
+  });
   const [{ x, y }, setPosition] = useState<Position>(
     (!isOffscreen && position) ||
       cascadePosition(id, processes, stackOrder, cascadeOffset) ||
