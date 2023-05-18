01c0d992103fec6a9de10975a09339bd4a2fe80c
Luki1235512 <Luki1235512@gmail.com>
2023-05-18 13:05:12 +0200

    Don't save window state without known position

--- components/system/Window/useWindowSize.ts
+++ components/system/Window/useWindowSize.ts
@@ -18,6 +18,7 @@ const useWindowSize = (id: string): WidnowSize => {
       setWindowStates((currentWindowStates) => ({
         ...currentWindowStates,
         [id]: {
+          position: currentWindowStates?.[id]?.position,
           size: {
             height: height + pxToNum(titleBar.height),
             width,
