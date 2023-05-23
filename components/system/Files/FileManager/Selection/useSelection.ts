9ae32adf9ff7f0b8c2a61228c10ef139b6748277
Luki1235512 <Luki1235512@gmail.com>
2023-05-23 12:42:47 +0200

    Selection properly inside file explorer

--- components/system/Files/FileManager/Selection/useSelection.ts
+++ components/system/Files/FileManager/Selection/useSelection.ts
@@ -1,5 +1,4 @@
 import type { Size } from "components/system/Window/RndWindow/useResizable";
-import type React from "react";
 import { useState } from "react";
 import type { Position } from "react-rnd";
 
@@ -25,21 +24,31 @@ const useSelection = (
   const { x, y } = position || {};
   const { height: h, width: w } = size || {};
   const onMouseMove: React.MouseEventHandler<HTMLElement> = ({
-    pageX,
-    pageY,
-  }) =>
-    setSize({
-      width: pageX - (x || 0),
-      height: pageY - (y || 0),
-    });
-  const onMouseDown: React.MouseEventHandler<HTMLElement> = ({
-    target,
-    pageX,
-    pageY,
+    clientX,
+    clientY,
   }) => {
-    if (target === containerRef?.current) {
+    const { x: targetX = 0, y: targetY = 0 } =
+      containerRef.current?.getBoundingClientRect() || {};
+
+    setSize({
+      width: clientX - targetX - (x || 0),
+      height: clientY - targetY - (y || 0),
+    });
+  };
+  const onMouseDown: React.MouseEventHandler<HTMLElement> = ({
+    clientX,
+    clientY,
+    target,
+  }) => {
+    if (target === containerRef.current) {
+      const { x: targetX = 0, y: targetY = 0 } =
+        containerRef.current.getBoundingClientRect();
+
       setSize({} as Size);
-      setPosition({ x: pageX, y: pageY });
+      setPosition({
+        x: clientX - targetX,
+        y: clientY - targetY,
+      });
     }
   };
   const hasSize = typeof w === "number" && typeof h === "number";
@@ -54,8 +63,8 @@ const useSelection = (
         height: `${Math.abs(Number(h))}px`,
         width: `${Math.abs(Number(w))}px`,
         transform: `translate(
-          ${Number(x) + (Number(w) < 0 ? Number(w) : 0)}px,
-          ${Number(y) + (Number(h) < 0 ? Number(h) : 0)}px)`,
+            ${Number(x) + (Number(w) < 0 ? Number(w) : 0)}px,
+            ${Number(y) + (Number(h) < 0 ? Number(h) : 0)}px)`,
       }
     : {};
 
