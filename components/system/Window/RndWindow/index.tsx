5dfc9bdc6ec37dcaff815ffe08d402191b3436f6
Luki1235512 <Luki1235512@gmail.com>
2023-05-29 13:18:42 +0200

    Don't set explicit all for pointers

--- components/system/Window/RndWindow/index.tsx
+++ components/system/Window/RndWindow/index.tsx
@@ -72,7 +72,7 @@ const RndWindow = ({ children, id, zIndex }: RndWindowProps): JSX.Element => {
     <Rnd
       ref={rndRef}
       style={{
-        pointerEvents: minimized ? "none" : "all",
+        pointerEvents: minimized ? "none" : undefined,
         zIndex,
       }}
       {...rndProps}
