aa11c820a430884a248ff19e14b7ed33068c3f08
Luki1235512 <Luki1235512@gmail.com>
2023-06-30 12:32:10 +0200

    Load jsdos files dynamically

--- components/apps/JSDOS/useDosCI.ts
+++ components/apps/JSDOS/useDosCI.ts
@@ -36,28 +36,44 @@ const useDosCI = (
     linkElement,
     processes: { [id]: { closing = false } = {} },
   } = useProcesses();
-  const [dosCI, setDosCI] = useState<CommandInterface>();
+  const [dosCI, setDosCI] = useState<Record<string, CommandInterface>>({});
 
   useEffect(() => {
-    if (dosInstance && !dosCI && fs && url) {
+    const closeBundle = (bundleUrl: string, closeInstance = false): void => {
+      dosCI[bundleUrl]?.persist().then((saveZip) =>
+        mkdirRecursive(SAVE_PATH, () => {
+          const saveName = `${basename(bundleUrl)}${saveExtension}`;
+
+          fs?.writeFile(join(SAVE_PATH, saveName), Buffer.from(saveZip), () => {
+            if (closeInstance) dosInstance?.stop();
+            updateFolder(SAVE_PATH, saveName);
+          });
+        })
+      );
+    };
+
+    if (dosInstance && fs && url && !dosCI[url]) {
       fs.readFile(url, async (_urlError, urlContents = EMPTY_BUFFER) => {
         const bundleURL = bufferToUrl(await addJsDosConfig(urlContents, fs));
 
         fs.readFile(
           join(SAVE_PATH, `${basename(url)}${saveExtension}`),
           (saveError, saveContents = EMPTY_BUFFER) => {
+            const [currentUrl] = Object.keys(dosCI);
             let optionalChangesUrl = "";
 
             if (!saveError) {
               optionalChangesUrl = bufferToUrl(saveContents);
             }
 
+            if (currentUrl) closeBundle(currentUrl);
+
             dosInstance.run(bundleURL, optionalChangesUrl).then((ci) => {
               const canvas = containerRef.current?.querySelector("canvas");
 
               if (canvas instanceof HTMLCanvasElement) {
                 linkElement(id, "peekElement", canvas);
-                setDosCI(ci);
+                setDosCI({ [url]: ci });
                 appendFileToTitle(url);
                 cleanUpBufferUrl(bundleURL);
                 if (optionalChangesUrl) cleanUpBufferUrl(optionalChangesUrl);
@@ -68,24 +84,8 @@ const useDosCI = (
         );
       });
     }
-
     return () => {
-      if (closing && dosCI && fs && url) {
-        dosCI.persist().then((saveZip) =>
-          mkdirRecursive(SAVE_PATH, () => {
-            const saveName = `${basename(url)}${saveExtension}`;
-
-            fs.writeFile(
-              join(SAVE_PATH, saveName),
-              Buffer.from(saveZip),
-              () => {
-                dosInstance?.stop();
-                updateFolder(SAVE_PATH, saveName);
-              }
-            );
-          })
-        );
-      }
+      if (closing) closeBundle(url, closing);
     };
   }, [
     appendFileToTitle,
@@ -101,7 +101,7 @@ const useDosCI = (
     url,
   ]);
 
-  return dosCI;
+  return dosCI[url];
 };
 
 export default useDosCI;
