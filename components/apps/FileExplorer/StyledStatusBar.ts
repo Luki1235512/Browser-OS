5b28ff2ca7fd278344f90a96d46fd0a2f430052d
Luki1235512 <Luki1235512@gmail.com>
2023-07-14 17:29:50 +0200

    Status bar for File Explorer

---
+++ components/apps/FileExplorer/StyledStatusBar.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const StyledStatusBar = styled.footer`
+  align-items: center;
+  background-color: rgb(230, 20, 230);
+  color: #fff;
+  display: flex;
+  height: 23px;
+  font-size: 12px;
+  font-weight: 100;
+  padding: 0 5px;
+  width: 100%;
+
+  div {
+    padding: 0 10px;
+    margin-top: -1px;
+
+    &::after {
+      border-right-width: 1px;
+      border-right-color: #fff;
+      border-right-style: solid;
+      bottom: 6px;
+      content: "";
+      height: 11px;
+      margin-left: 10px;
+      position: absolute;
+    }
+  }
+`;
+
+export default StyledStatusBar;
