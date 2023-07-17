eac250874b61908d41de6c5bf5f324cc8c1cff44
Luki1235512 <Luki1235512@gmail.com>
2023-07-17 11:08:53 +0200

    Allow switching to empty folders in File explorer

--- components/system/Files/FileManager/useFolder.ts
+++ components/system/Files/FileManager/useFolder.ts
@@ -181,6 +181,8 @@ const useFolder = (
               ...currentSortOrder,
               [directory]: Object.keys(sortedFiles),
             }));
+          } else {
+            setFiles({});
           }
         } catch (error) {
           if ((error as ApiError).code === "ENOENT") {
