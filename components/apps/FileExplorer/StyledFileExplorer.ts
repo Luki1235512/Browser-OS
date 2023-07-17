64bc341760e5778875197a58b1b4a9190d2b3f8a
Luki1235512 <Luki1235512@gmail.com>
2023-07-17 11:53:12 +0200

    Adjust entry styling file explorer

--- components/apps/FileExplorer/StyledFileExplorer.ts
+++ components/apps/FileExplorer/StyledFileExplorer.ts
@@ -1,9 +1,19 @@
+import StyledFileEntry from "components/system/Files/Views/Icon/StyledFileEntry";
 import StyledFileManager from "components/system/Files/Views/Icon/StyledFileManager";
 import styled from "styled-components";
 
 const StyledFileExplorer = styled.div`
   ${StyledFileManager} {
+    column-gap: 2px;
+    padding-left: 5px;
+    padding-right: 5px;
     height: calc(100% - 42px - 23px);
+  }
+
+  ${StyledFileEntry} {
+    &::before {
+      border: 0 !important;
+    }
   }
 `;
 
