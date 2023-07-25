20423b72b21f5c1e0c2a64f0e6c3db092b8e7e02
Luki1235512 <Luki1235512@gmail.com>
2023-07-25 10:02:52 +0200

    Contain styling when it makes sense

--- components/system/Menu/StyledMenu.ts
+++ components/system/Menu/StyledMenu.ts
@@ -13,6 +13,7 @@ const StyledMenu = styled(motion.nav)<StyledMenuProps>`
   box-shadow: 1px 1px 1px hsla(300, 80%, 60%, 70%),
     2px 2px 2px hsla(300, 100%, 60%, 70%);
   color: rgb(255, 255, 255);
+  contain: layout;
   font-size: 12px;
   height: fit-content;
   padding: 4px 2px;
