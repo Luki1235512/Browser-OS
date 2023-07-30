030f867a44b7f2c9e8a7ab8bfe27464f72885854
Luki1235512 <Luki1235512@gmail.com>
2023-07-31 00:23:39 +0200

    Fix tooltip/menu for http(s) url's on shortcuts

--- components/system/Files/FileEntry/index.tsx
+++ components/system/Files/FileEntry/index.tsx
@@ -201,6 +201,7 @@ const FileEntry = ({
     if (isShortcut) {
       if (comment) return comment;
       if (url) {
+        if (url.startsWith("http:") || url.startsWith("https:")) return url;
         return `Location: ${basename(url, extname(url))} (${dirname(url)})`;
       }
       return "";
