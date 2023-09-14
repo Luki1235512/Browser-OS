fe8574a48c6b302006e965aec118f9a6a907435b
Luki1235512 <Luki1235512@gmail.com>
2023-09-14 17:29:48 +0200

    Statusbar for IDE

--- components/apps/MonacoEditor/StyledMonacoEditor.ts
+++ components/apps/MonacoEditor/StyledMonacoEditor.ts
@@ -1,10 +1,15 @@
 import { overrideSubMenuStyling } from "components/apps/MonacoEditor/functions";
+import StyledWindow from "components/system/Window/StyledWindow";
 import styled from "styled-components";
 
 const StyledMonacoEditor = styled.div.attrs({ onBlur: overrideSubMenuStyling })`
   width: 100%;
   height: 100%;
-  color: ${({ theme }) => theme.colors.text};
+
+  ${StyledWindow} & {
+    height: ${({ theme }) =>
+      `calc(100% - ${theme.sizes.titleBar.height} - 31px) !important`};
+  }
 `;
 
 export default StyledMonacoEditor;
