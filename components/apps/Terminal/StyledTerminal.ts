ba2a8ccdd672560dd339f25fd91ab60876bde412
Luki1235512 <Luki1235512@gmail.com>
2023-09-19 17:58:39 +0200

    Better resizing for Terminal

--- components/apps/Terminal/StyledTerminal.ts
+++ components/apps/Terminal/StyledTerminal.ts
@@ -6,8 +6,14 @@ const StyledTerminal = styled.div`
   width: 100%;
   height: 100%;
 
+  .terminal {
+    height: 100% !important;
+  }
+
   .xterm-viewport {
     ${ScrollBars(DEFAULT_SCROLLBAR_WIDTH)};
+
+    width: 100% !important;
   }
 `;
 
