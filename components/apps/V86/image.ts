0de91b397dc90a65912aee9a87ea15617cf86fff
Luki1235512 <luki1235512@gmail.com>
2023-12-08 20:17:07 +0100

    Remove no-bitwise from .eslintrc.json

--- components/apps/V86/image.ts
+++ components/apps/V86/image.ts
@@ -14,6 +14,7 @@ const SUPPORTED_FLOPPY_TYPES = new Set([
 ]);
 
 const isFloppyImage = (size: number): boolean =>
+  // eslint-disable-next-line no-bitwise
   SUPPORTED_FLOPPY_TYPES.has(size >> 10) && (size & 0x3ff) === 0;
 
 export const getImageType = (size: number): string =>
