07e0219e4a618acee2ee139c1585efd542272d2d
Luki1235512 <luki1235512@gmail.com>
2024-01-31 23:26:09 +0100

    Added offscreen canvas for Chrome

--- styles/defaultTheme/wallpaper.ts
+++ styles/defaultTheme/wallpaper.ts
@@ -1,8 +1,12 @@
 import vantaWaves from "components/system/Desktop/Wallpapers/vantaWaves";
 
 const wallpaper = vantaWaves({
-  color: 0xbe4da7,
+  color: 0xf886d4,
+  colorCycleSpeed: 5,
   forceAnimate: true,
+  // hue: 200,
+  lightness: 15,
+  saturation: 35,
   shininess: 30,
   waveHeight: 15,
   waveSpeed: 0.7,
