eeedb5d64fae6cf268a929f8f63f0537b9a416e0
Luki1235512 <luki1235512@gmail.com>
2024-02-05 18:02:27 +0100

    Added basic ffmpeg transcoding

---
+++ utils/ffmpeg/types.ts
@@ -0,0 +1,32 @@
+export type IFFmpegInstance = {
+  FS: (
+    command: string,
+    fileName: string,
+    fileData?: Buffer
+  ) => Uint8Array | void;
+  load: () => Promise<void>;
+  run: (...commandArgs: string[]) => void;
+};
+
+export type IFFmpegLog = {
+  type: string;
+  message: string;
+};
+
+type FFmpegConfig = {
+  corePath: string;
+  log: boolean;
+  logger: (log: IFFmpegLog) => void;
+  mainName: string;
+};
+
+export type FFmpegTranscodeFile = [string, Buffer];
+
+declare global {
+  interface Window {
+    FFmpeg: {
+      createFFmpeg: (config: FFmpegConfig) => IFFmpegInstance;
+    };
+    FFmpegInstance: IFFmpegInstance;
+  }
+}
