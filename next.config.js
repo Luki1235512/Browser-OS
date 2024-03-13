973e9a8caa570cfe7031af6cbcbf2838ee36c62d
Luki1235512 <luki1235512@gmail.com>
2024-03-13 15:07:05 +0100

    Switch to swc compiler

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const nextConfig = {
+  compiler: {
+    reactRemoveProperties: true,
+    removeConsole: true,
+    styledComponents: true,
+  },
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
