7e14e69794f66984ac0dd730f6321d26f54f9c68
Luki1235512 <luki1235512@gmail.com>
2024-05-02 08:23:04 +0200

    vanta optimizations

--- components/system/Desktop/Wallpapers/vantaWaves/config.ts
+++ components/system/Desktop/Wallpapers/vantaWaves/config.ts
@@ -1,13 +1,25 @@
 export const config = {
+  camera: {
+    far: 400,
+    fov: 30,
+    near: 0.1,
+  },
   color: 0xf886d4,
   // colorCycleSpeed: 5,
   forceAnimate: true,
+  hh: 50,
   hue: 310,
   lightness: 50,
+  material: {
+    options: {
+      wireframe: false,
+    },
+  },
   saturation: 30,
   shininess: 35,
   waveHeight: 15,
   waveSpeed: 0.25,
+  ww: 50,
 };
 
 export const disableControls = {
