d29d1cd30d4660b65dccbf0ff4ca99a730686920
Luki1235512 <luki1235512@gmail.com>
2024-06-17 15:57:24 +0200

    Dynamic clock worker name

--- components/system/Taskbar/Clock/index.tsx
+++ components/system/Taskbar/Clock/index.tsx
@@ -1,12 +1,16 @@
 import type { LocaleTimeDate } from "components/system/Taskbar/Clock/functions";
 import StyledClock from "components/system/Taskbar/Clock/StyledClock";
+import useClockContextMenu from "components/system/Taskbar/Clock/useClockContextMenu";
 import { useSession } from "contexts/session";
 import useWorker from "hooks/useWorker";
 import { useCallback, useState } from "react";
 
-import useClockContextMenu from "./useClockContextMenu";
-
 type ClockWorkerResponse = LocaleTimeDate | "source";
+
+const ClockSourceMap = {
+  local: "Local",
+  ntp: "Server",
+};
 
 const Clock: FC = () => {
   const [now, setNow] = useState<LocaleTimeDate>({} as LocaleTimeDate);
@@ -19,9 +23,9 @@ const Clock: FC = () => {
           "components/system/Taskbar/Clock/clock.worker",
           import.meta.url
         ),
-        { name: "Clock" }
+        { name: `Clock (${ClockSourceMap[clockSource]})` }
       ),
-    []
+    [clockSource]
   );
   const updateTime = useCallback(
     ({ data, target: clockWorker }: MessageEvent<ClockWorkerResponse>) => {
