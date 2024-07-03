8513cee7053fe78a6229ff67cd4d64082b755958
Luki1235512 <luki1235512@gmail.com>
2024-07-03 18:41:55 +0200

    Shortcuts and DnD for Run dialog

--- components/system/Dialog/Run.tsx
+++ components/system/Dialog/Run.tsx
@@ -1,19 +1,24 @@
 import type { ComponentProcessProps } from "components/system/Apps/RenderComponent";
 import StyledButton from "components/system/Dialog/StyledButton";
 import StyledRun from "components/system/Dialog/StyledRun";
-import { getProcessByFileExtension } from "components/system/Files/FileEntry/functions";
+import {
+  getProcessByFileExtension,
+  getShortcutInfo,
+} from "components/system/Files/FileEntry/functions";
+import useFileDrop from "components/system/Files/FileManager/useFileDrop";
 import { useFileSystem } from "contexts/fileSystem";
 import { useProcesses } from "contexts/process";
 import processDirectory from "contexts/process/directory";
 import { useSession } from "contexts/session";
 import { extname } from "path";
 import { useCallback, useEffect, useRef, useState } from "react";
+import { SHORTCUT_EXTENSION } from "utils/constants";
 
 const OPEN_ID = "open";
 
 const Run: FC<ComponentProcessProps> = () => {
   const { open, close, processes: { Run: runProcess } = {} } = useProcesses();
-  const { exists, stat } = useFileSystem();
+  const { exists, readFile, stat } = useFileSystem();
   const { foregroundId } = useSession();
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [isInputFocused, setIsInputFocused] = useState(true);
@@ -22,29 +27,46 @@ const Run: FC<ComponentProcessProps> = () => {
     async (resource?: string) => {
       if (!resource) return;
 
-      if (await exists(resource)) {
-        const stats = await stat(resource);
+      const [resourcePid, ...resourceUrl] = resource.split(" ");
+      const resourcePath =
+        resourceUrl.length > 0 ? resourceUrl.join("") : resourcePid;
+
+      if (await exists(resourcePath)) {
+        const stats = await stat(resourcePath);
 
         if (stats.isDirectory()) {
-          open("FileExplorer", { url: resource }, "");
+          open("FileExplorer", { url: resourcePath }, "");
+        } else if (resourcePid && resourceUrl.length > 0) {
+          open(resourcePid, { url: resourcePath });
         } else {
-          const basePid = getProcessByFileExtension(extname(resource));
+          const extension = extname(resourcePath);
 
-          if (basePid) open(basePid, { url: resource });
+          if (extension === SHORTCUT_EXTENSION) {
+            const { pid, url } = getShortcutInfo(await readFile(resourcePath));
+
+            if (pid) open(pid, { url });
+          } else {
+            const basePid = getProcessByFileExtension(extension);
+
+            if (basePid) open(basePid, { url: resourcePath });
+          }
         }
       } else if (
         Object.keys(processDirectory).some(
-          (processName) => processName.toLowerCase() === resource.toLowerCase()
+          (processName) =>
+            processName.toLowerCase() === resourcePath.toLowerCase()
         )
       ) {
-        open(resource);
+        open(resourcePath);
       } else {
-        throw new Error(`${resource} not found`);
+        throw new Error(
+          `Cannot find '${resource}'. Make sure you typed the name correctly, and then try again.`
+        );
       }
 
       close("Run");
     },
-    [close, exists, open, stat]
+    [close, exists, open, readFile, stat]
   );
 
   useEffect(() => {
@@ -53,8 +75,17 @@ const Run: FC<ComponentProcessProps> = () => {
     }
   }, [foregroundId]);
 
+  useEffect(() => {
+    if (runProcess?.url && inputRef.current) {
+      inputRef.current.value = `${inputRef.current.value.trimEnd()} ${
+        runProcess.url.includes(" ") ? `"${runProcess.url}"` : runProcess.url
+      }`.trim();
+      setIsEmptyInput(false);
+    }
+  }, [runProcess?.url]);
+
   return (
-    <StyledRun>
+    <StyledRun {...useFileDrop({ id: "Run" })}>
       <figure>
         <img alt="Run" src="/System/Icons/32x32/run.webp" />
         <figcaption>
