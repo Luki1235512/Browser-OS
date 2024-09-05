6f8eedcde1204ba1dc25e723c5df676b538c3fdc
Luki1235512 <Luki1235512@gmail.com>
2024-09-05 20:04:34 +0200

    Dimmer wallpapers

--- components/system/Desktop/Wallpapers/useWallpaper.ts
+++ components/system/Desktop/Wallpapers/useWallpaper.ts
@@ -29,6 +29,11 @@ const cssFit: Record<WallpaperFit, string> = {
   tile: "",
 };
 
+const BRIGHT_WALLPAPERS: Record<string, `${number}%`> = {
+  COASTAL_LANDSCAPE: "85%",
+  HEXELLS: "85%",
+};
+
 const WALLPAPER_WORKERS: Record<string, () => Worker> = {
   COASTAL_LANDSCAPE: (): Worker =>
     new Worker(
@@ -93,6 +98,15 @@ const useWallpaper = (
         CoastalLandscape(desktopRef.current);
       } else {
         setWallpaper("VANTA");
+      }
+
+      if (BRIGHT_WALLPAPERS[wallpaperImage]) {
+        desktopRef.current
+          ?.querySelector("canvas")
+          ?.setAttribute(
+            "style",
+            `filter: brightness(${BRIGHT_WALLPAPERS[wallpaperImage]})`
+          );
       }
     }
   }, [
