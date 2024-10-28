9059a57310207c15a4edb2e3f70de5ed4429103e
Luki1235512 <Luki1235512@gmail.com>
2024-10-28 16:14:06 +0100

    Dont need to export this

--- components/apps/Webamp/functions.ts
+++ components/apps/Webamp/functions.ts
@@ -272,7 +272,7 @@ type MetadataProvider = (url: string) => MetadataGetter;
 const removeCData = (string = ""): string =>
   string.replaceAll(/<!\[CDATA\[|]]>/g, "");
 
-export const streamingMetadataProviders: Record<string, MetadataProvider> = {
+const streamingMetadataProviders: Record<string, MetadataProvider> = {
   "somafm.com": (url: string) => async () => {
     const { pathname } = new URL(url);
     const [channel] = pathname.replace("/", "").split("-");
