5aee3454e698e5bba53afc8685f55b319a5cf706
Luki1235512 <luki1235512@gmail.com>
2025-05-26 18:33:44 +0200

    Simpler memo'd component for apps

--- components/apps/MonacoEditor/index.tsx
+++ components/apps/MonacoEditor/index.tsx
@@ -1,16 +1,19 @@
-import ContainerComponent from "components/apps/AppContainer";
+import AppContainer from "components/apps/AppContainer";
 import StatusBar from "components/apps/MonacoEditor/StatusBar";
 import StyledMonacoEditor from "components/apps/MonacoEditor/StyledMonacoEditor";
 import useMonaco from "components/apps/MonacoEditor/useMonaco";
 import type { ComponentProcessProps } from "components/system/Apps/RenderComponent";
 
-const MonacoEditor: FC<ComponentProcessProps> = ({ id }) =>
-  ContainerComponent(
-    id,
-    useMonaco,
-    StyledMonacoEditor,
-    undefined,
-    <StatusBar id={id} />
-  );
+const MonacoEditor: FC<ComponentProcessProps> = ({ id }) => (
+  <>
+    <AppContainer
+      StyledComponent={StyledMonacoEditor}
+      id={id}
+      useHook={useMonaco}
+    >
+      <StatusBar id={id} />
+    </AppContainer>
+  </>
+);
 
 export default MonacoEditor;
