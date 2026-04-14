21b1a7cc305104532b32cd033846af13b089ad81
Luki1235512 <luki1235512@gmail.com>
2026-04-14 19:21:48 +0200

    Dialog button improvements

--- components/system/Dialogs/StyledButton.ts
+++ components/system/Dialogs/StyledButton.ts
@@ -1,37 +1,41 @@
 import styled from "styled-components";
 
-type StyledButtonProps = {
-  $active?: boolean;
-};
+const BASE_LINE_HEIGHT = 21;
 
-const StyledButton = styled.button<StyledButtonProps>`
+const StyledButton = styled.button`
   background-color: rgb(235, 76, 250);
-  border: ${({ $active }) =>
-    $active ? "2px solid rgb(215, 56, 230)" : "1px solid rgb(215, 56, 230)"};
+  border: 1px solid rgb(215, 56, 230);
+  display: grid;
   font-family: ${({ theme }) => theme.formats.systemFont};
   font-size: 12px;
   height: 23px;
-  transition: all 0.25s ease;
+  line-height: ${BASE_LINE_HEIGHT}px;
+  transition: background-color 0.25s ease;
   width: 73px;
 
-  &:focus {
+  &:focus,
+  &.focus {
     border: 2px solid rgb(215, 56, 230);
+    line-height: ${BASE_LINE_HEIGHT - 2}px;
   }
 
   &:hover {
     background-color: rgb(239, 169, 245);
     border: 1px solid rgb(219, 149, 225);
+    line-height: ${BASE_LINE_HEIGHT}px;
   }
 
   &:active {
     background-color: rgb(249, 179, 255);
     border: 1px solid rgb(215, 56, 230);
+    line-height: ${BASE_LINE_HEIGHT}px;
     transition: none;
   }
 
   &:disabled {
     background-color: rgb(204, 204, 204);
     border: 1px solid rgb(210, 200, 210);
+    line-height: ${BASE_LINE_HEIGHT}px;
   }
 `;
 
