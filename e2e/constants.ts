989c909f451781e57751662c9aa26a1991225226
Luki1235512 <luki1235512@gmail.com>
2026-06-05 09:26:38 +0200

    Update bug link

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -69,7 +69,9 @@ export const WEBGL_HEADLESS_NOT_SUPPORTED_BROWSERS = new Set(["firefox"]);
 export const SCREEN_CAPTURE_NOT_WORKING_BROWSERS = new Set(["webkit"]);
 export const FILE_DRAG_NOT_WORKING_BROWSERS = new Set(["webkit"]);
 export const MULTI_SELECT_NOT_WORKING_BROWSERS = new Set(["webkit"]);
-export const FOCUS_STATE_NOT_WORKING_BROWSERS = new Set(["webkit"]);
+export const FOCUS_STATE_NOT_WORKING_BROWSERS = new Set([
+  "webkit", // https://bugs.webkit.org/show_bug.cgi?id=22261
+]);
 
 export const FILE_MENU_ITEMS = [
   /^Open$/,
