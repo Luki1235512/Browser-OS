a9e522c4f17ae57bf22bd13470ded2c1d9442a02
Luki1235512 <luki1235512@gmail.com>
2026-06-25 16:14:02 +0200

    Use old unlock define logic

--- components/apps/MonacoEditor/useMonaco.ts
+++ components/apps/MonacoEditor/useMonaco.ts
@@ -59,20 +59,16 @@ const useMonaco = ({
 
     return newModel as Monaco.editor.ITextModel;
   }, [createModelUri, monaco?.editor, prependFileToTitle, readFile, url]);
-  const lockTimerRef = useRef<number>();
   const loadFile = useCallback(async () => {
     if (monaco && editor && url.startsWith("/")) {
       unlockGlobal("define");
 
-      editor.onDidChangeModel(() => {
-        window.clearTimeout(lockTimerRef.current);
-        lockTimerRef.current = window.setTimeout(
-          () => lockGlobal("define"),
-          MILLISECONDS_IN_SECOND
-        );
-      });
       editor.getModel()?.dispose();
       editor.setModel(await createModel());
+      window.setTimeout(
+        () => lockGlobal("define"),
+        2.5 * MILLISECONDS_IN_SECOND
+      );
     }
 
     prependFileToTitle(basename(url || DEFAULT_TEXT_FILE_SAVE_PATH));
